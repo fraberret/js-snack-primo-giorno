b7f44d9285fcdfaeddd52c4d1a8ac2f8e5283730
fraberret <f.berretta02@gmail.com>
2024-02-16 17:10:59 +0100

    add EX 2/C

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,12 @@ console.log(PI);
 
     // EX 2/B
 
-    const PI = 3.1416
-    
+    //const PI = 3.1416
+
+    // EX 2/C
+
+    let radius = 8
+    let circle = radius * 2 * PI
+    console.log(circle);
+
     
